b952876d9cbe959423221550ac6db4fe52576327
ManupaDev <manupadevsuite@gmail.com>
2024-10-26 23:11:51 +0530

    feat: session-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Button from "./Button";
 import Hero from "./Hero";
+import Navigation from "./Navigation";
 
 function App() {
+  const name = "Manupa";
+  const cartCount = 2;
+
   return (
     <div>
+      <Navigation name={name} cartCount={cartCount} />
       <Hero />
+      <div className="p-4">
+        <Button>Buy Now</Button>
+      </div>
     </div>
   );
 }
